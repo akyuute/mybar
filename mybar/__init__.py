c03bcecbfe7dc6b56ec8758ee0c8a1673dda4fab
SleepyAbsol <trackmenot10+github@gmail.com>
2022-11-17 22:50:29 -0800

    Update username, copyright info

--- mybar/__init__.py
+++ mybar/__init__.py
@@ -4,17 +4,17 @@ mybar
 
 An asynchronous status bar with an intuitive, highly customizable API.
 
-:copyright: (c) 2022 by derek-yeetr.
+:copyright: (c) 2022 by SleepyAbsol.
 :license: MIT, see LICENSE for more details.
 """
 
 __title__ = 'mybar'
 __description__ = "An async status bar with a highly customizable API."
-__url__ = "https://github.com/derek-yeetr/mybar"
+__url__ = "https://github.com/sleepyabsol/mybar"
 __version__ = '0.2'
-__author__ = "derek-yeetr"
+__author__ = "SleepyAbsol"
 __license__ = 'MIT'
-__copyright__ = 'Copyright (c) 2022 derek-yeetr'
+__copyright__ = 'Copyright (c) 2021-2022 SleepyAbsol'
 
 
 from . import field_funcs
