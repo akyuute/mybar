11954bca33a8a7c692be29a89aab08ef77227c88
SleepyAbsol <trackmenot10+github@gmail.com>
2022-11-29 14:38:46 -0800

    Add CLI argument help

--- mybar/cli.py
+++ mybar/cli.py
@@ -11,115 +11,6 @@ ConfigSpec: TypeAlias = dict
 
 PROG = __package__
 
-class MyFormatter(HelpFormatter):
-    def _format_actions_usage(self, actions, groups):
-        # find group indices and identify actions in groups
-        group_actions = set()
-        inserts = {}
-        for group in groups:
-            if not group._group_actions:
-                raise ValueError(f'empty group {group}')
-
-            try:
-                start = actions.index(group._group_actions[0])
-            except ValueError:
-                continue
-            else:
-                end = start + len(group._group_actions)
-                if actions[start:end] == group._group_actions:
-                    for action in group._group_actions:
-                        group_actions.add(action)
-                    if not group.required:
-                        if start in inserts:
-                            inserts[start] += ' ['
-                        else:
-                            inserts[start] = '['
-                        if end in inserts:
-                            inserts[end] += ']'
-                        else:
-                            inserts[end] = ']'
-                    else:
-                        if start in inserts:
-                            inserts[start] += ' ('
-                        else:
-                            inserts[start] = '('
-                        if end in inserts:
-                            inserts[end] += ')'
-                        else:
-                            inserts[end] = ')'
-                    for i in range(start + 1, end):
-                        inserts[i] = '|'
-
-        # collect all actions format strings
-        parts = []
-        for i, action in enumerate(actions):
-
-            # suppressed arguments are marked with None
-            # remove | separators for suppressed arguments
-            if action.help is SUPPRESS:
-                parts.append(None)
-                if inserts.get(i) == '|':
-                    inserts.pop(i)
-                elif inserts.get(i + 1) == '|':
-                    inserts.pop(i + 1)
-
-            # produce all arg strings
-            elif not action.option_strings:
-                default = self._get_default_metavar_for_positional(action)
-                part = self._format_args(action, default)
-
-                # if it's in a group, strip the outer []
-                if action in group_actions:
-                    if part[0] == '[' and part[-1] == ']':
-                        part = part[1:-1]
-
-                # add the action string to the list
-                parts.append(part)
-
-            # produce the first way to invoke the option in brackets
-            else:
-################option_string = action.option_strings[0]
-                option_string = '|'.join(action.option_strings)
-
-                # if the Optional doesn't take a value, format is:
-                #    -s or --long
-                if action.nargs == 0:
-                    part = action.format_usage()
-
-                # if the Optional takes a value, format is:
-                #    -s ARGS or --long ARGS
-                else:
-                    default = self._get_default_metavar_for_optional(action)
-                    args_string = self._format_args(action, default)
-                    part = '%s %s' % (option_string, args_string)
-
-                # make it look optional if it's not required or in a group
-                if not action.required and action not in group_actions:
-                    part = '[%s]' % part
-
-                # add the action string to the list
-                parts.append(part)
-
-        # insert things at the necessary indices
-        for i in sorted(inserts, reverse=True):
-            parts[i:i] = [inserts[i]]
-
-        # join all the action items with spaces
-        text = ' '.join([item for item in parts if item is not None])
-
-        # clean up separators for mutually exclusive groups
-        open = r'[\[(]'
-        close = r'[\])]'
-        text = re.sub(r'(%s) ' % open, r'\1', text)
-        text = re.sub(r' (%s)' % close, r'\1', text)
-        text = re.sub(r'%s *%s' % (open, close), r'', text)
-        text = re.sub(r'\(([^|]*)\)', r'\1', text)
-        text = text.strip()
-
-        # return the text
-        return text
-
-
 
 class UnrecoverableError(Exception):
     def __init__(self, msg: str) -> None:
@@ -131,12 +22,10 @@ class UsageError(UnrecoverableError):
 
 
 class Parser(ArgumentParser):
-    #TODO: Override print_usage!
     def __init__(self) -> None:
         super().__init__(
             prog=PROG,
             argument_default=SUPPRESS,
-            # formatter_class=MyFormatter,
         )
         self.add_arguments()
 
@@ -173,7 +62,7 @@ class Parser(ArgumentParser):
             metavar=('FIELDNAME1', 'FIELDNAME2'),
             dest='field_order',
             # type=(lambda f: f.split(',', 1)),
-            help="",
+            help="A list of fields to be displayed. Not valid with --format/-m options.",
         )
 
         self.add_argument(
@@ -182,55 +71,70 @@ class Parser(ArgumentParser):
             nargs='+',
             metavar=("FIELDNAME1='ICON1'", "FIELDNAME2='ICON2'"),
             dest='icon_pairs',
-            help="",
+            help="A mapping of field names to icons.",
         )
 
         self.add_argument(
             '-m', '--format',
             metavar="'FORMAT_STRING'",
             dest='fmt',
-            help="",
+            help=(
+                "A curly-brace-delimited format string. "
+                "Not valid with --fields/-f options."
+            ),
         )
 
         self.add_argument(
             '-s', '--separator',
             metavar="'FIELD_SEPARATOR'",
             dest='separator',
-            help="",
+            help=(
+                "The character used for joining fields. "
+                "Only valid with --field/-f options."
+            ),
         )
 
         self.add_argument(
             '-r', '--refresh',
             type=float,
             dest='refresh_rate',
-            help="",
+            help=(
+                "The bar's refresh rate in cycles/second."
+            ),
         )
 
         self.add_argument(
             '--config', '-c',
             metavar='FILE',
             dest='config_file',
-            help="",
+            help=(
+                "The config file to use for default settings."
+            ),
         )
 
         self.add_argument(
             '--join-empty', '-j',
             action='store_true',
             dest='join_empty_fields',
-            help="",
+            help=(
+                "Include empty field contents instead of hiding them. "
+                "Only valid with --field/-f options."
+            ),
         )
 
         self.add_argument(
             '--once', '-o',
             action='store_true',
             dest='run_once',
-            help="",
+            help=(
+                "Only run the bar once rather than continuously."
+            ),
         )
 
         self.add_argument(
             '--debug',
             action='store_true',
-            help="",
+            help="Use debug mode.",
         )
 
 
