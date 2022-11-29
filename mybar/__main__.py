bcac7791acceaf70d5ab70c07467075b6779b67f
SleepyAbsol <trackmenot10+github@gmail.com>
2022-11-29 00:38:26 -0800

    Actually add __main__.py

--- mybar/__main__.py
+++ mybar/__main__.py
@@ -6,7 +6,20 @@ from typing import NoReturn
 
 def main() -> NoReturn:
     '''Run the command line utility.'''
-    cfg = cli.gather_config()
+    # cfg = cli.gather_config()
+
+    parser = cli.Parser()
+    try:
+        options = parser.parse_args()
+    except cli.UnrecoverableError as e:
+        parser.error(e.msg)
+    
+    try:
+        cfg = Config(opts=options)
+    except OSError as e:
+        errmsg = f"{parser.prog}: error: {e}"
+        parser.exit(1, message=errmsg + '\n')
+
     bar = cfg.to_bar()
     bar.run()
 
