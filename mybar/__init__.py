4c3ba097e12c80523a307b8452b8406b501cabb1
sleepyabsol <trackmenot10@gmail.com>
2023-02-28 01:36:15 -0800

    Bump version; Correct copyright

--- mybar/__init__.py
+++ mybar/__init__.py
@@ -4,14 +4,14 @@ mybar
 
 An asynchronous status bar with an intuitive, highly customizable API.
 
-:copyright: (c) 2022 by SleepyAbsol.
+:copyright: (c) 2021-present by SleepyAbsol.
 :license: MIT, see LICENSE for more details.
 """
 
 __title__ = 'mybar'
 __description__ = "An async status bar with a highly customizable API."
 __url__ = "https://github.com/sleepyabsol/mybar"
-__version__ = '0.4'
+__version__ = '0.5'
 __author__ = "SleepyAbsol"
 __license__ = 'MIT'
 __copyright__ = "Copyright (c) 2021-present SleepyAbsol"
