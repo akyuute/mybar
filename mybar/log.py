96cd8b80868af6305a280f161502c98ec8fad8d7
lonelyabsol <trackmenot10@gmail.com>
2023-05-31 05:00:33 -0700

    Add basic logger

--- mybar/log.py
+++ mybar/log.py
@@ -3,9 +3,15 @@ __all__ = (
 )
 
 
+import logging
+import os.path
+
+from .constants import CONFIG_FILE
+
+
 logging.basicConfig(
     level='DEBUG',
-    filename=os.path.expanduser('~/.mybar.log'),
+    filename=os.path.dirname(CONFIG_FILE) + '/mybar.log',
     filemode='w',
     datefmt='%Y-%m-%d_%H:%M:%S.%f',
     format='[{asctime}] ({levelname}:{name}) {message}',
