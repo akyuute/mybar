6b05550ec2cb72382e6368cff99948a27925dc80
Sam <trackmenot10@gmail.com>
2023-09-15 23:16:22 -0700

    Move run(), write_initial_config() to __init__; Revamp init imports

--- mybar/__init__.py
+++ mybar/__init__.py
@@ -5,7 +5,8 @@ mybar
 
 *Craft highly customizable status bars with ease.*
 
-**mybar** is a versatile status bar library and tool written in Python.
+**mybar** is a versatile status bar library and command line tool
+written in Python.
 
 
 :copyright: (c) 2021-present by LonelyAbsol.
@@ -13,7 +14,7 @@ mybar
 """
 
 __title__ = 'mybar'
-__description__ = "An async status bar with a highly customizable API."
+__description__ = "Craft highly customizable status bars with ease."
 __url__ = "https://github.com/lonelyabsol/mybar"
 __version__ = '0.8'
 __author__ = "LonelyAbsol"
@@ -21,8 +22,64 @@ __license__ = 'MIT'
 __copyright__ = "Copyright (c) 2021-present LonelyAbsol"
 
 
-from .constants import *
-from . import utils
-from .bar import *
-from .field import *
+__all__ = (
+    'Bar',
+    'BarConfig',
+    'CONFIG_FILE',
+    'DEBUG',
+    'Field',
+    'run',
+    'write_initial_config'
+)
+
+from .constants import CONFIG_FILE, DEBUG
+from .bar import Bar, BarConfig
+from .field import Field
+
+from os import PathLike
+from typing import NoReturn
+
+
+def run(once: bool = False, file: PathLike = None) -> NoReturn | None:
+    '''
+    Generate a new :class:`Bar` from a config file and run it in STDOUT.
+    Ask to write the file if it doesn't exist.
+
+    :param once: Print the bar only once, defaults to ``False``
+    :type once: :class:`bool`
+
+    :param file: The config file to source,
+        defaults to :obj:`CONFIG_FILE`
+    :type file: :class:`PathLike`
+    '''
+##    import __main__ as possibly_repl
+##    if not hasattr(possibly_repl, '__file__'):
+##        # User is in a REPL
+    try:
+
+        try:
+            bar = Bar.from_file(file)
+        except FileNotFoundError as e:
+            _p_ = __package__
+            msg = (
+                f"Try running `{_p_}.write_initial_config()`"
+                f" if this is your first time using {_p_}."
+            )
+            e.add_note(msg)
+            raise e from None
+
+        bar.run(once=once)
+
+    except KeyboardInterrupt:
+        print()
+        return
+
+del PathLike, NoReturn
+
+
+def write_initial_config() -> None:
+    '''
+    '''
+    BarConfig.write_with_approval(CONFIG_FILE)
+
 
