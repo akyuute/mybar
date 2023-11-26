219ecfa2d8dfe7501d757743bfc614d520c14f53
sleepyabsol <trackmenot10@gmail.com>
2023-11-26 02:29:09 -0800

    Remove possible invalid option

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ autodoc_class_signature = 'separated'
 
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build']
 
 
 
@@ -51,5 +51,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
